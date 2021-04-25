b0e72159c178ee8beeabd850016b717e1eff5ea9
Khang <khangvippro357@gmail.com>
2021-04-25 21:39:23 +0700

    Capnhat25

--- src/Hooks/ShowHome.js
+++ src/Hooks/ShowHome.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import "../Styles/Home.css";
+import Anh from "../img/image 1.png";
 
 export default function ShowHome() {
   const [show, setShow] = useState(false);
@@ -16,16 +17,65 @@ export default function ShowHome() {
     <div id="show-home">
       <div className={show ? "show-more mt-4" : "show-less d-none"}>
         <div className="show-more-header">
-          <div className="line-middle"></div>
-          <h1>Đây là Show more</h1>
-          <div className="line-middle"></div>
+          {/* <div className="line-middle"></div> */}
+          <h3>Game Recommended</h3>
+          {/* <div className="line-middle"></div> */}
         </div>
-        <p>Hello mấy cưng</p>
+        <div className="show-more-list">
+          <div className="show-more-game-items">
+            <img src={Anh}></img>
+            <p>50k+ Views</p>
+            <span className="text-game-items" style={{display: "block"}}>Leaguage of Legends</span>
+            <a href="https://youtube.com" target="_blank" className="game-items">MOBA</a>
+          </div>
+          <div className="show-more-game-items">
+            <img src={Anh}></img>
+            <p>50k+ Views</p>
+            <span className="text-game-items" style={{display: "block"}}>Leaguage of Legends</span>
+            <a href="https://youtube.com" target="_blank" className="game-items">MOBA</a>
+          </div>
+          <div className="show-more-game-items">
+            <img src={Anh}></img>
+            <p>50k+ Views</p>
+            <span className="text-game-items" style={{display: "block"}}>Leaguage of Legends</span>
+            <a href="https://youtube.com" target="_blank" className="game-items">MOBA</a>
+          </div>
+          <div className="show-more-game-items">
+            <img src={Anh}></img>
+            <p>50k+ Views</p>
+            <span className="text-game-items" style={{display: "block"}}>Leaguage of Legends</span>
+            <a href="https://youtube.com" target="_blank" className="game-items">MOBA</a>
+          </div>
+          <div className="show-more-game-items">
+            <img src={Anh}></img>
+            <p>50k+ Views</p>
+            <span className="text-game-items" style={{display: "block"}}>Leaguage of Legends</span>
+            <a href="https://youtube.com" target="_blank" className="game-items">MOBA</a>
+          </div>
+          <div className="show-more-game-items">
+            <img src={Anh}></img>
+            <p>50k+ Views</p>
+            <span className="text-game-items" style={{display: "block"}}>Leaguage of Legends</span>
+            <a href="https://youtube.com" target="_blank" className="game-items">MOBA</a>
+          </div>
+          <div className="show-more-game-items">
+            <img src={Anh}></img>
+            <p>50k+ Views</p>
+            <span className="text-game-items" style={{display: "block"}}>Leaguage of Legends</span>
+            <a href="https://youtube.com" target="_blank" className="game-items">MOBA</a>
+          </div>
+          <div className="show-more-game-items">
+            <img src={Anh}></img>
+            <p>50k+ Views</p>
+            <span className="text-game-items" style={{display: "block"}}>Leaguage of Legends</span>
+            <a href="https://youtube.com" target="_blank" className="game-items">MOBA</a>
+          </div>
+        </div>
       </div>
       <div className="show-more-game">
         <div className="line-middle"></div>
         <button onClick={handleShowmore}>
-          {show ? ("Show-less" && <i class="fa fa-arrow-up"></i>): ("Show-more" && <i class="fa fa-arrow-down"></i>)} 
+          {show ? (<div>Show Less <i class="fa fa-arrow-up"></i></div>): (<div>Show more <i class="fa fa-arrow-down"></i></div>)} 
         </button>
         <div className="line-middle"></div>
       </div>
