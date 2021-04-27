85f598a6912e2ef6edbc1b1903c4bfbfd39af580
Khang <khangvippro357@gmail.com>
2021-04-27 08:07:31 +0700

    update27

--- src/Hooks/ShowHome.js
+++ src/Hooks/ShowHome.js
@@ -10,72 +10,256 @@ export default function ShowHome() {
     setShow((show) => !show);
     // setShowText("show-less");
   };
-  
-
 
   return (
     <div id="show-home">
-      <div className={show ? "show-more mt-4" : "show-less d-none"}>
-        <div className="show-more-header">
-          {/* <div className="line-middle"></div> */}
-          <h3>Game Recommended</h3>
-          {/* <div className="line-middle"></div> */}
+      <div className="show-more-header">
+        {/* <div className="line-middle"></div> */}
+        <h3>Game Recommended</h3>
+        {/* <div className="line-middle"></div> */}
+      </div>
+      <div className="show-more-list">
+          <div className="show-more-game-items">
+            <img src={Anh}></img>
+            <p>50k+ Views</p>
+            <span className="text-game-items" style={{ display: "block" }}>
+              Leaguage of Legends
+            </span>
+            <a
+              href="https://youtube.com"
+              target="_blank"
+              className="game-items"
+            >
+              MOBA
+            </a>
+          </div>
+          <div className="show-more-game-items">
+            <img src={Anh}></img>
+            <p>50k+ Views</p>
+            <span className="text-game-items" style={{ display: "block" }}>
+              Leaguage of Legends
+            </span>
+            <a
+              href="https://youtube.com"
+              target="_blank"
+              className="game-items"
+            >
+              MOBA
+            </a>
+          </div>
+          <div className="show-more-game-items">
+            <img src={Anh}></img>
+            <p>50k+ Views</p>
+            <span className="text-game-items" style={{ display: "block" }}>
+              Leaguage of Legends
+            </span>
+            <a
+              href="https://youtube.com"
+              target="_blank"
+              className="game-items"
+            >
+              MOBA
+            </a>
+          </div>
+          <div className="show-more-game-items">
+            <img src={Anh}></img>
+            <p>50k+ Views</p>
+            <span className="text-game-items" style={{ display: "block" }}>
+              Leaguage of Legends
+            </span>
+            <a
+              href="https://youtube.com"
+              target="_blank"
+              className="game-items"
+            >
+              MOBA
+            </a>
+          </div>
+          <div className="show-more-game-items">
+            <img src={Anh}></img>
+            <p>50k+ Views</p>
+            <span className="text-game-items" style={{ display: "block" }}>
+              Leaguage of Legends
+            </span>
+            <a
+              href="https://youtube.com"
+              target="_blank"
+              className="game-items"
+            >
+              MOBA
+            </a>
+          </div>
+          <div className="show-more-game-items">
+            <img src={Anh}></img>
+            <p>50k+ Views</p>
+            <span className="text-game-items" style={{ display: "block" }}>
+              Leaguage of Legends
+            </span>
+            <a
+              href="https://youtube.com"
+              target="_blank"
+              className="game-items"
+            >
+              MOBA
+            </a>
+          </div>
+          <div className="show-more-game-items">
+            <img src={Anh}></img>
+            <p>50k+ Views</p>
+            <span className="text-game-items" style={{ display: "block" }}>
+              Leaguage of Legends
+            </span>
+            <a
+              href="https://youtube.com"
+              target="_blank"
+              className="game-items"
+            >
+              MOBA
+            </a>
+          </div>
+          <div className="show-more-game-items">
+            <img src={Anh}></img>
+            <p>50k+ Views</p>
+            <span className="text-game-items" style={{ display: "block" }}>
+              Leaguage of Legends
+            </span>
+            <a
+              href="https://youtube.com"
+              target="_blank"
+              className="game-items"
+            >
+              MOBA
+            </a>
+          </div>
         </div>
+      <div className={show ? "show-more mt-4" : "show-less d-none"}>
         <div className="show-more-list">
           <div className="show-more-game-items">
             <img src={Anh}></img>
             <p>50k+ Views</p>
-            <span className="text-game-items" style={{display: "block"}}>Leaguage of Legends</span>
-            <a href="https://youtube.com" target="_blank" className="game-items">MOBA</a>
+            <span className="text-game-items" style={{ display: "block" }}>
+              Leaguage of Legends
+            </span>
+            <a
+              href="https://youtube.com"
+              target="_blank"
+              className="game-items"
+            >
+              MOBA
+            </a>
           </div>
           <div className="show-more-game-items">
             <img src={Anh}></img>
             <p>50k+ Views</p>
-            <span className="text-game-items" style={{display: "block"}}>Leaguage of Legends</span>
-            <a href="https://youtube.com" target="_blank" className="game-items">MOBA</a>
+            <span className="text-game-items" style={{ display: "block" }}>
+              Leaguage of Legends
+            </span>
+            <a
+              href="https://youtube.com"
+              target="_blank"
+              className="game-items"
+            >
+              MOBA
+            </a>
           </div>
           <div className="show-more-game-items">
             <img src={Anh}></img>
             <p>50k+ Views</p>
-            <span className="text-game-items" style={{display: "block"}}>Leaguage of Legends</span>
-            <a href="https://youtube.com" target="_blank" className="game-items">MOBA</a>
+            <span className="text-game-items" style={{ display: "block" }}>
+              Leaguage of Legends
+            </span>
+            <a
+              href="https://youtube.com"
+              target="_blank"
+              className="game-items"
+            >
+              MOBA
+            </a>
           </div>
           <div className="show-more-game-items">
             <img src={Anh}></img>
             <p>50k+ Views</p>
-            <span className="text-game-items" style={{display: "block"}}>Leaguage of Legends</span>
-            <a href="https://youtube.com" target="_blank" className="game-items">MOBA</a>
+            <span className="text-game-items" style={{ display: "block" }}>
+              Leaguage of Legends
+            </span>
+            <a
+              href="https://youtube.com"
+              target="_blank"
+              className="game-items"
+            >
+              MOBA
+            </a>
           </div>
           <div className="show-more-game-items">
             <img src={Anh}></img>
             <p>50k+ Views</p>
-            <span className="text-game-items" style={{display: "block"}}>Leaguage of Legends</span>
-            <a href="https://youtube.com" target="_blank" className="game-items">MOBA</a>
+            <span className="text-game-items" style={{ display: "block" }}>
+              Leaguage of Legends
+            </span>
+            <a
+              href="https://youtube.com"
+              target="_blank"
+              className="game-items"
+            >
+              MOBA
+            </a>
           </div>
           <div className="show-more-game-items">
             <img src={Anh}></img>
             <p>50k+ Views</p>
-            <span className="text-game-items" style={{display: "block"}}>Leaguage of Legends</span>
-            <a href="https://youtube.com" target="_blank" className="game-items">MOBA</a>
+            <span className="text-game-items" style={{ display: "block" }}>
+              Leaguage of Legends
+            </span>
+            <a
+              href="https://youtube.com"
+              target="_blank"
+              className="game-items"
+            >
+              MOBA
+            </a>
           </div>
           <div className="show-more-game-items">
             <img src={Anh}></img>
             <p>50k+ Views</p>
-            <span className="text-game-items" style={{display: "block"}}>Leaguage of Legends</span>
-            <a href="https://youtube.com" target="_blank" className="game-items">MOBA</a>
+            <span className="text-game-items" style={{ display: "block" }}>
+              Leaguage of Legends
+            </span>
+            <a
+              href="https://youtube.com"
+              target="_blank"
+              className="game-items"
+            >
+              MOBA
+            </a>
           </div>
           <div className="show-more-game-items">
             <img src={Anh}></img>
             <p>50k+ Views</p>
-            <span className="text-game-items" style={{display: "block"}}>Leaguage of Legends</span>
-            <a href="https://youtube.com" target="_blank" className="game-items">MOBA</a>
+            <span className="text-game-items" style={{ display: "block" }}>
+              Leaguage of Legends
+            </span>
+            <a
+              href="https://youtube.com"
+              target="_blank"
+              className="game-items"
+            >
+              MOBA
+            </a>
           </div>
         </div>
       </div>
       <div className="show-more-game">
         <div className="line-middle"></div>
         <button onClick={handleShowmore}>
-          {show ? (<div>Show Less <i class="fa fa-arrow-up"></i></div>): (<div>Show more <i class="fa fa-arrow-down"></i></div>)} 
+          {show ? (
+            <div>
+              Show Less <i class="fa fa-arrow-up"></i>
+            </div>
+          ) : (
+            <div>
+              Show more <i class="fa fa-arrow-down"></i>
+            </div>
+          )}
         </button>
         <div className="line-middle"></div>
       </div>
