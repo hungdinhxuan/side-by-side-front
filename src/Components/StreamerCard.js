fd1e6be70de384df13c815477de7e6a5fee5be6b
GM <dinhhungGM>
2021-05-13 23:06:55 +0700

    split code from Pages/Streamer/index.js to StreamerCard in Components

---
+++ src/Components/StreamerCard.js
@@ -0,0 +1,43 @@
+import React from "react";
+import {
+  Card,
+  Button,
+  CardImg,
+  CardTitle,
+  CardText,
+  CardColumns,
+  CardSubtitle,
+  CardBody,
+} from "reactstrap";
+
+const StreamerCard = ({ streamer }) => {
+  return (
+    <div className="col-3" key={streamer._id}>
+      <CardColumns>
+        <Card style={{ width: "270px", height: "380px" }}>
+          <CardImg
+            top
+            width="100%"
+            height="50%"
+            src={streamer.avatar}
+            alt="Card image cap"
+          />
+          <CardBody>
+            <CardTitle tag="h5">{streamer._id}</CardTitle>
+            <CardSubtitle
+              tag="h6"
+              className="mb-2 text-muted"
+              style={{ h6: "red" }}
+            >
+              {streamer.price}đ/1h
+            </CardSubtitle>
+            <CardText>Adu</CardText>
+            <Button>Thuê ngay</Button>
+          </CardBody>
+        </Card>
+      </CardColumns>
+    </div>
+  );
+};
+
+export default StreamerCard;
--- src/Pages/Streamer/index.js
+++ src/Pages/Streamer/index.js
@@ -1,87 +1,45 @@
 import React, { useState, useEffect } from "react";
 
-import {
-  Card,
-  Button,
-  CardImg,
-  CardTitle,
-  CardText,
-  CardColumns,
-  CardSubtitle,
-  CardBody,
-} from "reactstrap";
-
-import { makeStyles } from '@material-ui/core/styles';
-import Pagination from '@material-ui/lab/Pagination';
-import Typography from '@material-ui/core/Typography';
+import Pagination from "@material-ui/lab/Pagination";
+import Typography from "@material-ui/core/Typography";
 
 import { useDispatch, useSelector } from "react-redux";
 import { getStreamerByPage } from "../../actions/streamer";
-import { useParams } from "react-router-dom";
-import Anhmau from '../../img/header_bg.jpg';
+
+import Anhmau from "../../img/header_bg.jpg";
+
+import StreamerCard from "../../Components/StreamerCard";
 
 export default function Streamer() {
   const dispatch = useDispatch();
-  const { dulieu, isLoading, error } = useSelector((state) => state.streamer);
+  let { dulieu } = useSelector((state) => state.streamer);
   const [page, setPage] = useState(1);
+
+  const handleChange = (event, value) => {
+    setPage(value);
+  };
 
   useEffect(() => {
     dispatch(getStreamerByPage(page));
   }, []);
-
-  if (isLoading) {
-    console.log(dulieu);
-  }
-
-  if (error) {
-    console.log(error);
-  }
-  const handleChange = (event, value) => {
-    setPage(value);
-  }
-  
-
   return (
     <div className="streamer">
-      <div className="container row" style={{backgroundImage: {Anhmau}}}>
-        {dulieu &&
-          dulieu.map((item) => 
-            (
-              <div className="col-3">
-                <CardColumns>
-                  <Card style={{width: "270px", height: "380px"}}>
-                    <CardImg
-                      top
-                      width="100%"
-                      height="50%"
-                      src={item.avatar}
-                      alt="Card image cap"
-                    />
-                    <CardBody>
-                      <CardTitle tag="h5">{item._id}</CardTitle>
-                      <CardSubtitle
-                        tag="h6"
-                        className="mb-2 text-muted"
-                        style={{ h6: "red" }}
-                      >
-                        {item.price}đ/1h
-                      </CardSubtitle>
-                      <CardText>hùng gà</CardText>
-                      <Button>Thuê ngay</Button>
-                    </CardBody>
-                  </Card>
-                </CardColumns>
-              </div>
-            )
-          ) }
+      <div className="container row" style={{ backgroundImage: { Anhmau } }}>
+        {dulieu && dulieu.map((item) => <StreamerCard streamer={item} />)}
       </div>
       {/* <div style={{width: "22%", margin: "10px auto"}}>
       <Pagination coun
       t={10} color="primary" size="large" />
       </div> */}
-      <div style={{width: "22%", margin: "10px auto"}}>
-      <Typography>Page: {page}</Typography>
-      <Pagination count={10} page={page} size="large" color="primary" onChange={handleChange}/>
+      <div style={{ width: "22%", margin: "10px auto" }}>
+        <Typography>Page: {page}</Typography>
+        <Pagination
+          count={10}
+          page={page}
+          size="large"
+          color="primary"
+          onChange={handleChange}
+        />
       </div>
     </div>
   );
