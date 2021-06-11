d33a217721faecb1b2959f3ee24128655634567c
dinhhungGM <dinhhungGM@gmail.com>
2021-06-11 09:10:11 +0700

    Change Services

--- src/Services/socket.js
+++ src/Services/socket.js
@@ -1,4 +1,6 @@
 import io from "socket.io-client";
+import {getCookie} from "./handleCookie"
 
-
-export const socket = io('https://rent-me-now.herokuapp.com');
+export const socket = io('https://rent-me-now.herokuapp.com', {
+    query: getCookie("token")
+});
