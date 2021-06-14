8b0e4761af1d3bc2e7b57f618805399ea888bd15
dinhhungGM <dinhhungGM@gmail.com>
2021-06-14 20:41:05 +0700

    Remove lib unnecessary

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React  from "react";
 import ReactDOM from "react-dom";
-import '@progress/kendo-theme-default/dist/all.css';
 import "./Styles";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
