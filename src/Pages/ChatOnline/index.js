11111c492e6d61c076999f48e8e0cb2a685fbb06
dinhhungGM <dinhhungGM@gmail.com>
2021-06-14 20:28:35 +0700

    Remove lib unnecessary

--- src/Pages/ChatOnline/index.js
+++ src/Pages/ChatOnline/index.js
@@ -1,50 +1,50 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import { Chat } from "@progress/kendo-react-conversational-ui";
+// import React from "react";
+// import ReactDOM from "react-dom";
+// import { Chat } from "@progress/kendo-react-conversational-ui";
 
-export default function ChatOnline() {
-  const MessageTemplate = (props) => {
-    return (
-      <div className="k-bubble">
-        <div>{props.item.text}</div>
-      </div>
-    );
-  };
+// export default function ChatOnline() {
+//   const MessageTemplate = (props) => {
+//     return (
+//       <div className="k-bubble">
+//         <div>{props.item.text}</div>
+//       </div>
+//     );
+//   };
 
-  const user = {
-    id: 1,
-    name: "Jane",
-    avatarUrl: "https://via.placeholder.com/24/008000/008000.png",
-  };
-  const user2 = {
-    id: 2,
-    name: "Mina",
-    avatarUrl: "https://via.placeholder.com/24/008000/008000.png",
-  };
+//   const user = {
+//     id: 1,
+//     name: "Jane",
+//     avatarUrl: "https://via.placeholder.com/24/008000/008000.png",
+//   };
+//   const user2 = {
+//     id: 2,
+//     name: "Mina",
+//     avatarUrl: "https://via.placeholder.com/24/008000/008000.png",
+//   };
 
-  const [messages, setMessages] = React.useState([]);
+//   const [messages, setMessages] = React.useState([]);
 
-  const addNewMessage = (event) => {
-    setMessages([...messages, event.message]);
-  };
+//   const addNewMessage = (event) => {
+//     setMessages([...messages, event.message]);
+//   };
 
-  return (
-    <div>
-      <Chat
-        user={user}
-        messages={messages}
-        onMessageSend={addNewMessage}
-        width={600}
-        messageTemplate={MessageTemplate}
-      />
+//   return (
+//     <div>
+//       <Chat
+//         user={user}
+//         messages={messages}
+//         onMessageSend={addNewMessage}
+//         width={600}
+//         messageTemplate={MessageTemplate}
+//       />
       
-      <Chat
-        messages={messages}
-        user={user2}
-        width={600}
-        onMessageSend={addNewMessage}
-        messageTemplate={MessageTemplate}
-      />
-    </div>
-  );
-}
+//       <Chat
+//         messages={messages}
+//         user={user2}
+//         width={600}
+//         onMessageSend={addNewMessage}
+//         messageTemplate={MessageTemplate}
+//       />
+//     </div>
+//   );
+// }
